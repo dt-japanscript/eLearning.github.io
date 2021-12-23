371c355afddf8033ab779ff4e82e9766d7ba003d
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2021-12-23 15:10:54 +0900

    Create Dark mode for sidebar

--- js/main.js
+++ js/main.js
@@ -25,8 +25,12 @@ function truncateString(str, num) {
 /* Sidebar Mini */
 var toggleBtn = document.querySelector('.sidebarMini__button');
 var sidebarMini = document.querySelector('.sidebarMini');
+var switchBtn = document.querySelector('#checkbox');
 
 toggleBtn.addEventListener('click', function () {
     sidebarMini.classList.toggle('is-opened');
-}
-)
+});
+
+switchBtn.addEventListener('click', function () {
+    document.querySelector('body').classList.toggle('darkMode');
+});
